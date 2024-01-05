eb5142a4053e4218f32316cf445b427b003829a6
VictorRudakov <victor-rudakov@yandex.ru>
2024-01-05 15:30:15 +0300

    - Added tests: ToUpperLimit and ToLowerLimit - Removed test MaxSize_1

--- GTest/tests.cpp
+++ GTest/tests.cpp
@@ -7,6 +7,8 @@ using TestQueue = typename SafeQueue<int>;
 using Result = typename TestQueue::Result;
 using Item = typename TestQueue::Item;
 
+//--------------------------------------------------------------------------
+
 TEST(SafeQueueTest, Closed) {
     TestQueue q(0);
     q.close();
@@ -20,27 +22,38 @@ TEST(SafeQueueTest, Closed) {
     EXPECT_EQ(item, -1);
 }
 
-TEST(SafeQueueTest, ZeroSize) {
+//--------------------------------------------------------------------------
+
+TEST(SafeQueueTest, ZeroMaxSize) {
     TestQueue q(0);
     EXPECT_EQ(q.get_closed(), true);
 }
 
-TEST(SafeQueueTest, MaxSize_1) {
-    TestQueue q(1);
-    EXPECT_EQ(q.get_max_size(), 1);
-    ASSERT_EQ(q.get_closed(), false);
-    Item item = -1;
-    EXPECT_EQ(q.pop(item), Result::Empty);
-    EXPECT_EQ(item, -1);
-    EXPECT_EQ(q.push(1), Result::Success);
-    EXPECT_EQ(q.push(2), Result::Full);
-    EXPECT_EQ(q.pop(item), Result::Success);
-    EXPECT_EQ(item, 1);
-    EXPECT_EQ(q.pop(item), Result::Empty);
-    EXPECT_EQ(q.push(2), Result::Success);
-    EXPECT_EQ(q.pop(item), Result::Success);
-    EXPECT_EQ(item, 2);
+//--------------------------------------------------------------------------
+
+TEST(SafeQueueTest, ToUpperLimit) {
+    TestQueue q(5);
+    for (auto i = 0; i < q.get_max_size(); ++i) {
+        ASSERT_EQ(q.push(i), Result::Success);
+    }
+    ASSERT_EQ(q.push(100), Result::Full);
 }
+
+//--------------------------------------------------------------------------
+
+TEST(SafeQueueTest, ToLowerLimit) {
+    TestQueue q(5);
+    for (auto i = 0; i < q.get_max_size(); ++i) {
+        ASSERT_EQ(q.push(i), Result::Success);
+    }
+    int value;
+    for (auto i = 0; i < q.get_max_size(); ++i) {
+        ASSERT_EQ(q.pop(value), Result::Success);
+    }
+    ASSERT_EQ(q.pop(value), Result::Empty);
+}
+
+//--------------------------------------------------------------------------
 
 TEST(SafeQueueTest, FIFO) {
     constexpr unsigned int QueueMaxSize = 1000;
@@ -61,6 +74,8 @@ TEST(SafeQueueTest, FIFO) {
     int item;
     EXPECT_EQ(q.pop(item), Result::Empty);
 }
+
+//--------------------------------------------------------------------------
 
 TEST(SafeQueueTest, find_if) {
     constexpr int QueueMaxSize = 10;
@@ -99,3 +114,4 @@ TEST(SafeQueueTest, find_if) {
     }
 }
 
+//--------------------------------------------------------------------------
