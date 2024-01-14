3de5e798d332c2ced068f75cf62904e154f4021f
VictorRudakov <victor-rudakov@yandex.ru>
2024-01-15 00:13:05 +0300

    Update main.c
    - Fixed code style

--- r4/r4_17/main.c
+++ r4/r4_17/main.c
@@ -31,7 +31,7 @@ int get_array(char name, int array[], int size)
 
 void print_array(char name, int array[], int size)
 {
-    if( array == 0 || size <= 0 )
+    if (array == 0 || size <= 0)
     {
         printf("\nInternal error: incorrect array '%c' print params", name);
         return;
@@ -66,37 +66,37 @@ void sort_array(int array[], int size)
 
 int main_1()
 {
-    const int SIZE_A = 3;
-    const int SIZE_B = 5;
-    const int SIZE_C = SIZE_A + SIZE_B;
+    const int Size_A = 3;
+    const int Size_B = 5;
+    const int Size_C = Size_A + Size_B;
 
-    int A[SIZE_A];
-    int B[SIZE_B];
-    int C[SIZE_C];
+    int A[Size_A];
+    int B[Size_B];
+    int C[Size_C];
 
-    if (!get_array('A', A, SIZE_A) || !get_array('B', B, SIZE_B))
+    if (!get_array('A', A, Size_A) || !get_array('B', B, Size_B))
     {
         return -1;
     }
 
     printf("\nArrays before sorting:");
-    print_array('A', A, SIZE_A);
-    print_array('B', B, SIZE_B);
+    print_array('A', A, Size_A);
+    print_array('B', B, Size_B);
 
-    for (int i=0; i < SIZE_A; i++)
+    for (int i=0; i < Size_A; i++)
         C[i] = A[i];
 
-    for (int i=0; i < SIZE_B; i++)
-        C[SIZE_A + i] = B[i];
+    for (int i=0; i < Size_B; i++)
+        C[Size_A + i] = B[i];
 
-    sort_array(A, SIZE_A);
-    sort_array(B, SIZE_B);
-    sort_array(C, SIZE_C);
+    sort_array(A, Size_A);
+    sort_array(B, Size_B);
+    sort_array(C, Size_C);
 
     printf("\nArrays after sorting:");
-    print_array('A', A, SIZE_A);
-    print_array('B', B, SIZE_B);
-    print_array('C', C, SIZE_C);
+    print_array('A', A, Size_A);
+    print_array('B', B, Size_B);
+    print_array('C', C, Size_C);
     printf("\n");
 
     system("pause");
@@ -104,12 +104,54 @@ int main_1()
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
+//    Удалить из линейного целочисленного массива все нули, пе-
+//    редвинув на их место следующие элементы без нарушения порядка их
+//    следования.
+
+int main_2()
+{
+    const int Size = 6;
+    int a[Size];
+    if (!get_array('a', a, Size))
+    {
+        return -1;
+    }
+
+    printf("\nArray before changes:");
+    print_array('a', a, Size);
+
+    for (int i = 0;i < Size; i++)
+    {
+        if (a[i] == 0)
+        {
+            // find first non-zerro
+            int non_zerro=i+1;
+            for (; non_zerro < Size; non_zerro++)
+            {
+                if (a[non_zerro] != 0)
+                    break;
+            }
+            if (non_zerro==Size)
+                break; // no non-zerro items
+
+            // swap zerro with non-zerro items
+            a[i] = a[non_zerro];
+            a[non_zerro] = 0;
+        }
+    }
+
+    printf("\nArray after changes:");
+    print_array('a', a, Size);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
 //    main_1();
-//do {
-      main_1();
-//} while(1);
+//    main_2();
+do {
+      main_2();
+} while(1);
     return 0;
 }
