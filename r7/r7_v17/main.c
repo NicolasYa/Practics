7ce0138e54dcd57f35dfc0a3ff2e6b6497c1794d
VictorRudakov <victor-rudakov@yandex.ru>
2024-01-17 03:00:58 +0300

    Update main.c
    - Added checks for stdlib function calls
    - Changed circle of reading to one call

--- r7/r7_v17/main.c
+++ r7/r7_v17/main.c
@@ -2,7 +2,7 @@
 
 int main() {
     FILE *file;
-    int n = 5; // количество чисел
+    const int n = 5; // количество чисел
     float numbers[n];
     float sum = 0;
     float average;
@@ -10,14 +10,20 @@ int main() {
     // Открытие бинарного файла для записи
     file = fopen("binary_file.bin", "wb");
     if (file == NULL) {
-        printf("Failed to open file.\n");
+        printf("Failed to open file for writing.\n");
         return 1;
     }
+
+    printf("binary_file.bin opened for writing\n");
 
     // Ввод чисел и запись в бинарный файл
     printf("Enter %d float numbers:\n", n);
     for (int i = 0; i < n; i++) {
-        scanf("%f", &numbers[i]);
+        if (scanf("%f", &numbers[i]) != 1)
+        {
+            printf("\nIncorrect value entered");
+            return 1;
+        }
         sum += numbers[i];
     }
 
@@ -28,8 +34,18 @@ int main() {
         numbers[i] -= average;
     }
 
+    printf("Average: %.2f\n", average);
+    printf("Normalized numbers:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%.2f ", numbers[i]);
+    }
+    printf("\n");
+
     // Запись вещественных чисел в бинарный файл
-    fwrite(numbers, sizeof(float), n, file);
+    if (fwrite(numbers, sizeof(float), n, file) != n) {
+        printf("Write to file failed.\n");
+        return 1;
+    }
 
     // Закрытие бинарного файла
     fclose(file);
@@ -37,21 +53,24 @@ int main() {
     // Открытие бинарного файла для чтения
     file = fopen("binary_file.bin", "rb");
     if (file == NULL) {
-        printf("Failed to open file.\n");
+        printf("Failed to open file for reading.\n");
+        return 1;
+    }
+    printf("binary_file.bin opened for reading\n");
+
+    // Чтение содержимого бинарного файла и вывод на экран
+    if (fread(numbers, sizeof(float), n, file) != n) {
+        printf("Read failed.\n");
         return 1;
     }
 
-    // Чтение содержимого бинарного файла и вывод на экран
-    printf("Content of binary file:\n");
+    printf("Content of binary_file.bin:\n");
     for (int i = 0; i < n; i++) {
-        float num;
-        fread(&num, sizeof(float), 1, file);
-        printf("%.2f ", num);
+        printf("%.2f ", numbers[i]);
     }
     printf("\n");
 
     // Закрытие бинарного файла
     fclose(file);
-
     return 0;
 }
